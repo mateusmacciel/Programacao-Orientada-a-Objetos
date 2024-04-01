f81f22c8593eeff615df4015831e7e91b5c57a0b
Mateus <mateusmacciel@gmail.com>
2024-04-01 01:15:00 -0300

    PBL03

--- pbl03.java
+++ pbl03.java
@@ -1,3 +1,51 @@
-public class pbl03 {
+class Cliente {
+    String nome;
+    double saldo;
     
+    public Cliente(String nome, double saldo) {
+        this.nome = nome;
+        this.saldo = saldo;
+    }
+
+    public void depositar(double valor) {
+        this.saldo = saldo + valor;
+    }
+
+    public void retirar(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo = saldo - valor;
+        } else {
+            System.out.println("Não possui saldo suficiente.");
+        }
+    }
+
+    public void imprimir() {
+        System.out.println(nome + " " + saldo);
+    }
+}
+
+class Banco {
+    public static void main (String[] args){
+        Cliente c1 = new Cliente("Jandira da Silva", 2500.00);
+        Cliente c2 = new Cliente("Sandra Rodrigues", 1800.00);
+        Cliente c3 = new Cliente("Luciana Teixeira", 5000.00);
+
+        c1.imprimir();
+        c2.imprimir();
+        c3.imprimir();
+
+        c1.retirar(1000);
+        c1.imprimir();
+
+        c2.retirar(2000);
+
+        c2.depositar(500);
+        c2.imprimir();
+
+        c2.retirar(2000);
+        c2.imprimir();
+
+        c3.depositar(1000);
+        c3.imprimir();
+    }
 }
